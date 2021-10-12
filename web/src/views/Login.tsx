79a90b166a6472321078899e1722043ca2d41521
Damian Kocjan <DamianoKPL@wp.pl>
2021-10-12 21:08:54 +0200

    add routes to app and login system

---
+++ web/src/views/Login.tsx
@@ -0,0 +1,144 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom'
+
+import { loginUser, useAuthDispatch, useAuthState } from '../auth';
+
+const LoginView: React.FC = () => {
+  const history = useHistory();
+  const [loginDetails, setLoginDetails] = useState({
+    strategy: 'local',
+    email: '',
+    password: '',
+  });
+
+  const dispatch = useAuthDispatch();
+  const { isLogged, loading, errorMessage } = useAuthState();
+
+  useEffect(() => {
+    if (isLogged) {
+      try {
+        history.goBack();
+      } catch {
+        history.push('/');
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLogged) {
+      try {
+        history.goBack();
+      } catch {
+        history.push('/');
+      }
+    }
+  }, [isLogged]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginDetails({
+      ...loginDetails,
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+
+  const handleLogin = async (
+    e: React.MouseEvent<HTMLButtonElement> | React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    try {
+      const res = await loginUser(dispatch, loginDetails);
+
+      if (errorMessage) {
+        console.error(errorMessage);
+      }
+
+      if (!res) return;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Logo"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+        </div>
+        <form className="mt-8 space-y-6" method="POST" onSubmit={handleLogin}>
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                value={loginDetails.email}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={loginDetails.password}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                Remember me
+              </label>
+            </div>
+
+            <div className="text-sm">
+              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+};
+
+export default LoginView;
---
+++ web/src/views/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundView: React.FC = () => {
+  return (
+    <div>
+      not found
+    </div>
+  )
+}
+
+export default NotFoundView
