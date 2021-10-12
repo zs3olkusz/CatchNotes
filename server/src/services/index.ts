303fdb7a41c023de44fbcbef2c59c77b776e9efd
Damian Kocjan <DamianoKPL@wp.pl>
2021-10-12 21:11:49 +0200

    add notes routes

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,7 +1,9 @@
-import { Application } from '../declarations';
-import users from './users/users.service';
+import { Application } from "../declarations";
+import notes from "./notes/notes.service";
+import users from "./users/users.service";
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
+  app.configure(notes);
   app.configure(users);
 }
