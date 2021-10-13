d504c5f41241748d88967fd27c1468aa22b700f4
Kacp3r3k <kacperek.drozynski@gmail.com>
2021-10-13 14:45:22 +0200

    about page

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -59,7 +59,7 @@ const Navbar: React.FC = () => {
                 FAQ
               </Link>
               <Link
-                to="/"
+                to="/about"
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
               >
                 About
@@ -149,7 +149,7 @@ const Navbar: React.FC = () => {
                   </Link>
 
                   <Link
-                    to="/"
+                    to="/about"
                     className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
                     <span>About</span>
---
+++ web/src/views/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad cumque
+      nesciunt! Unde iure dolor, consequuntur itaque voluptatum voluptate saepe
+      molestiae reiciendis excepturi necessitatibus, fuga voluptatem libero
+      quibusdam minus magnam.
+    </div>
+  );
+};
+
+export default About;
