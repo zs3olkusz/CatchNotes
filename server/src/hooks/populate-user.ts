cf5bce8b6ad18047e18b736934cfcf1e192e7e71
Damian Kocjan <DamianoKPL@wp.pl>
2021-10-15 18:07:13 +0200

    populate user to result

---
+++ server/src/hooks/populate-user.ts
@@ -0,0 +1,31 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+import { Hook, HookContext } from '@feathersjs/feathers';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default (options = {}): Hook => {
+  return async (context: HookContext): Promise<HookContext> => {
+    const { app, method, result, params } = context;
+
+    const addUser = async (data: any) => {
+      const user = await app.service('users').get(data.userId, params);
+
+      // Merge the data content to include the `user` object
+      return {
+        ...data,
+        user,
+      };
+    };
+
+    // In a find method we need to process the entire page
+    if (method === 'find') {
+      // Map all data to include the `user` information
+      context.result.data = await Promise.all(result.data.map(addUser));
+    } else {
+      // Otherwise just update the single result
+      context.result = await addUser(result);
+    }
+
+    return context;
+  };
+};
