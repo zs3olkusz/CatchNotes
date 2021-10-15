52e210d86b9a9daa152ceccf6dc28098dca93290
Damian Kocjan <DamianoKPL@wp.pl>
2021-10-15 21:05:55 +0200

    fixes

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -156,7 +156,7 @@ const Navbar: React.FC = () => {
                     <span>About</span>
                   </Link>
                 </div>
-                {isLogged ? (
+                {!isLogged ? (
                   <div>
                     <Link
                       to="/register"
@@ -181,7 +181,7 @@ const Navbar: React.FC = () => {
                       className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                       onClick={handleLogout}
                     >
-                      Register
+                      Logout
                     </Link>
                   </div>
                 )}
