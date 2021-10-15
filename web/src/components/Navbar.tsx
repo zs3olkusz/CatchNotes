a0aec9f9b353211f67f26e9d39ebdb7a82116014
siemowit <IDawid923@gmail.com>
2021-10-15 20:04:50 +0200

    added FAQ page

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -53,7 +53,7 @@ const Navbar: React.FC = () => {
                 <span>Home</span>
               </Link>
               <Link
-                to="/"
+                to="/faq"
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
               >
                 FAQ
@@ -142,7 +142,7 @@ const Navbar: React.FC = () => {
                   </Link>
 
                   <Link
-                    to="/"
+                    to="/faq"
                     className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
                     <span>Colab</span>
