f5c482cd352854ed0711ad3ecea0333e4dadba3d
GranysTL <84445857+GranysTL@users.noreply.github.com>
2021-10-15 21:23:22 +0200

    fix sign up button

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -86,7 +86,7 @@ const Navbar: React.FC = () => {
                 </Link>
                 <Link
                   to="/register"
-                  className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                  className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-green-400 to-blue-500 focus:from-pink-500 focus:to-yellow-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Sign up
                 </Link>
