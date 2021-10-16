038f6bdaef747c576f8ffe4d1db1adeac8994c64
Damian Kocjan <DamianoKPL@wp.pl>
2021-10-16 20:21:17 +0200

    please format your code

--- web/src/views/NoteList.tsx
+++ web/src/views/NoteList.tsx
@@ -22,22 +22,22 @@ const NoteListView: React.FC = () => {
   const { status, data, error } = useUserNotes(user.id);
 
   return (
-    <section className="text-gray-600 body-font overflow-hidden max-w-7xl mx-auto" >
-      <div className="container px-5 py-24 mx-auto" >
-        <div className="-my-8 ">
+    <section className="text-gray-600 body-font overflow-hidden max-w-7xl mx-auto">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="-my-8">
           {status === 'loading' ? (
             <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
               loading
             </h2>
           ) : status === 'error' ? (
             <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-              {error}
+              {error.message || error}
             </h2>
           ) : (
-            data.data.map((note: INote) => (
-              <Link 
+            data.data && data.data.map((note: INote) => (
+              <Link
                 to={`/notes/${note.id}`}
-                className="py-4 flex flex-wrap md:flex-nowrap mb-1 bg-gradient-to-r from-blue-500 via-pink-400 to-purple-500 rounded-lg "
+                className="py-4 flex flex-wrap md:flex-nowrap mb-1 bg-gradient-to-r from-blue-500 via-pink-400 to-purple-500 rounded-lg"
                 key={note.id}
               >
                 <div className="md:flex-grow">
@@ -47,22 +47,22 @@ const NoteListView: React.FC = () => {
                     </h2>
                   </div>
                   {/* {note.description && ( */}
-                    {/* <p className="leading-relaxed">{note.description}</p> */}
+                  {/* <p className="leading-relaxed">{note.description}</p> */}
                   {/* )} */}
                   <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col ">
-                  <p className="text-white text-base md:text-lg capitalize  mx-5" >
-                    <time
-                      dateTime={format(
-                        new Date(note.createdAt),
-                        "yyyy-MM-dd'T'HH:mm:ss"
-                      )}
-                    >
-                      {formatDistance(new Date(note.createdAt), new Date(), {
-                        addSuffix: true,
-                      })}
-                    </time>
-                  </p>
-                </div>
+                    <p className="text-white text-base md:text-lg capitalize mx-5">
+                      <time
+                        dateTime={format(
+                          new Date(note.createdAt),
+                          "yyyy-MM-dd'T'HH:mm:ss"
+                        )}
+                      >
+                        {formatDistance(new Date(note.createdAt), new Date(), {
+                          addSuffix: true,
+                        })}
+                      </time>
+                    </p>
+                  </div>
                 </div>
               </Link>
             ))
