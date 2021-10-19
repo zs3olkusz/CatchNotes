a9be9e8c23ab965de9c1ab3dfe3dbd724b463594
Damian Kocjan <DamianoKPL@wp.pl>
2021-10-20 00:57:02 +0200

    create message service

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -3,6 +3,7 @@ import users from './users/users.service';
 import notes from './notes/notes.service';
 import noteSections from './note-sections/note-sections.service';
 import quizAnswers from './quiz-answers/quiz-answers.service';
+import messages from './messages/messages.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
@@ -10,4 +11,5 @@ export default function (app: Application): void {
   app.configure(notes);
   app.configure(noteSections);
   app.configure(quizAnswers);
+  app.configure(messages);
 }
