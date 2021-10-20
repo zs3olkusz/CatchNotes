e261fabd1881dfd378464a9e77308cc3437e24d2
Akitsuki <92682598+Ak1tsu@users.noreply.github.com>
2021-10-20 06:12:37 -0700

    Fix collab typo

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar: React.FC = () => {
                 to="/"
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
               >
-                <span>Colab</span>
+                <span>Collab</span>
               </Link>
               <Link
                 to="/"
